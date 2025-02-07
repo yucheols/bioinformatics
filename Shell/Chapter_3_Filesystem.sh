29850781bee32befd0e86b1c51816ed7666d3b2f
Yucheol Shin <brongersmai2@gmail.com>
2025-02-07 14:27:39 -0500

    commit

---
+++ Shell/Chapter_3_Filesystem.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# print user name
+echo $USER
+
+# set environment variables & dealing with spaces
+export GREETING=Hello
+echo GREETING    # this just prints the word GREETING
+echo $GREETING   # this prints out the world Hello, which is saved within the environment variabe called GREETING
+
+# lets see what having a space between two words can do
+export GREETING=Hello everyone   # this will print out Hello only
+export GREETING2=Hello\ everyone # this prints out the two words propoerly
+
+echo $GREETING
+echo $GREETING2
