c42df5ec3a5a8e4b0565e7abaa3ba4a1f3a332ec
Yucheol Shin <brongersmai2@gmail.com>
2025-02-07 14:31:29 -0500

    commit

--- Shell/Chapter_3_Filesystem.sh
+++ Shell/Chapter_3_Filesystem.sh
@@ -9,8 +9,10 @@ echo GREETING    # this just prints the word GREETING
 echo $GREETING   # this prints out the world Hello, which is saved within the environment variabe called GREETING
 
 # lets see what having a space between two words can do
-export GREETING=Hello everyone   # this will print out Hello only
-export GREETING2=Hello\ everyone # this prints out the two words propoerly
+export GREETING=Hello everyone    # this will print out Hello only
+export GREETING2=Hello\ everyone  # this prints out the two words propoerly
+export GREETING3='Hello everyone' # this does the same thing
 
 echo $GREETING
 echo $GREETING2
+echo $GREETING3
